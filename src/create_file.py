38fdde4bc3aa595030d6196e15ff45c4252d80fa
moe-ad <adedigbamuhammed@gmail.com>
2024-11-03 19:46:43 +0100

    Let us see what will yet happen again

--- src/create_file.py
+++ src/create_file.py
@@ -1,4 +1,5 @@
 # This is a test script that creates a file which I can then upload as artifact on Github
 
-with open('../output_files/sample_file.txt', mode='w') as w:
+# with open('../output_files/sample_file.txt', mode='w') as w:
+with open('sample_file.txt', mode='w') as w:
     w.write('My name is Moe\nI just started as an employee at Ansys')
